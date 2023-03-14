d26a487146bcf011e0e6e7aa051c9c34e8b5af95
Anton Kolvakh <a.kolvakh@outlook.com>
2023-03-14 13:38:38 +0200

    CD-34 #comment change ec2 ami for London region

--- deployments/pr_haulin/env-hln/_layers/10.core/25.bastion/tf-variables.tf
+++ deployments/pr_haulin/env-hln/_layers/10.core/25.bastion/tf-variables.tf
@@ -12,5 +12,5 @@ variable "tf_framework_component_version" {
   description = "GIT tag or branch if no tag vailable, identifying terraform source code version being run. Set by Makefile Framework"
 }
 variable "ami" {
-  default = "ami-0fcda042dd8ae41c7"
+  default = "ami-07100047e41837f0e" //eu-west-2 //us-east-1 "ami-0fcda042dd8ae41c7"
 }
